fe191cccdf472749e04dfbbbba26e7e7c29fb710
Leonardo Oliveira <leonardo.oliveirasilva1005@gmail.com>
2021-04-20 02:33:55 -0300

    tests for create

--- spec/controllers/dns_records_controller_spec.rb
+++ spec/controllers/dns_records_controller_spec.rb
@@ -383,7 +383,142 @@ RSpec.describe Api::V1::DnsRecordsController, type: :controller do
   end
 
 
-  # describe '#create' do
-  #   # TODO
-  # end
+  describe '#create' do
+    context 'with an valid ip address and hostnames' do
+      let(:ip1) { '1.1.1.1' }
+      let(:lorem) { 'lorem.com' }
+      let(:ipsum) { 'ipsum.com' }
+      let(:dolor) { 'dolor.com' }
+      let(:amet) { 'amet.com' }
+      let(:sit) { 'sit.com' }
+
+      let(:payload1) do
+        {
+          dns_records: {
+            ip: ip1,
+            hostnames_attributes: [
+              {
+                hostname: lorem
+              },
+              {
+                hostname: ipsum
+              },
+              {
+                hostname: dolor
+              },
+              {
+                hostname: amet
+              }
+            ]
+          }
+        }.to_json
+      end
+
+      let(:expected_response) do
+        {
+          id: 52
+        }
+      end
+        
+      before :each do
+        request.accept = 'application/json'
+        request.content_type = 'application/json'
+
+        post(:create, body: payload1, format: :json)
+      end
+
+      it 'responds with created' do
+        expect(response).to have_http_status(:created)
+      end
+
+      it 'returns the id on response' do
+        expect(parsed_body).to eq expected_response
+      end
+
+      it 'save and guarantee that all the data is in db' do
+        dns_record = DnsRecord.where(ip: ip1).first
+        db_hostnames = DnsHostname.where(dns_record_id: dns_record.id).pluck(:hostname).to_json
+        
+        expected_hostnames = [
+          lorem,
+          ipsum,
+          dolor,
+          amet
+        ].to_json
+
+        expect(dns_record).not_to be_nil
+        expect(db_hostnames).to eq expected_hostnames
+      end
+    end
+
+    context 'try to create an duplicated ip address' do
+      let(:ip1) { '1.1.1.1' }
+      let(:lorem) { 'lorem.com' }
+
+      let(:payload1) do
+        {
+          dns_records: {
+            ip: ip1,
+            hostnames_attributes: [
+              {
+                hostname: lorem
+              }
+            ]
+          }
+        }.to_json
+      end
+
+      before :each do
+        request.accept = 'application/json'
+        request.content_type = 'application/json'
+
+        post(:create, body: payload1, format: :json)
+      end
+
+      it 'responds with conflict' do
+        request.accept = 'application/json'
+        request.content_type = 'application/json'
+
+        post(:create, body: payload1, format: :json)
+        expect(response).to have_http_status(:conflict)
+      end
+    end
+
+    context 'try to create with an invalid ip address' do
+      let(:ip1) { '1.1.1.' }
+      let(:lorem) { 'lorem.com' }
+
+      let(:payload1) do
+        {
+          dns_records: {
+            ip: ip1,
+            hostnames_attributes: [
+              {
+                hostname: lorem
+              }
+            ]
+          }
+        }.to_json
+      end
+
+      let(:expected_response) do
+        { errors: { ip: ['is invalid']} }
+      end
+
+      before :each do
+        request.accept = 'application/json'
+        request.content_type = 'application/json'
+
+        post(:create, body: payload1, format: :json)
+      end
+
+      it 'responds with bad request' do
+        expect(response).to have_http_status(:bad_request)
+      end
+
+      it 'responds with the field and the error to fix' do
+        expect(parsed_body).to eq expected_response
+      end
+    end
+  end
 end
